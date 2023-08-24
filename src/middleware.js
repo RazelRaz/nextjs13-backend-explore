d3d874d8ee4bc8e726533069a30411a988aa99c0
RazelRaz <razel16dcm@gmail.com>
2023-08-24 21:33:41 +0600

    conditional statement - middleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,11 +2,24 @@ import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export function middleware(req,res){
-    console.log("Middleware");
-}
+// export function middleware(req,res){
+//     console.log("Middleware");
+// }
 
-// specific path set & muliple path middleware
-export const config = {
-    matcher:['/api/:path*','/site']
+// // specific path set & muliple path middleware
+// export const config = {
+//     matcher:['/api/:path*','/site']
+// }
+
+
+
+//conditional statement - middleware
+export function middleware(req,res){
+    if(req.nextUrl.pathname.startsWith("/api/product")){
+        console.log("IM API Middleware");
+    } else if (req.nextUrl.pathname.startsWith("/dashboard")){
+        console.log("IM dashboard Middleware");
+    } else if (req.nextUrl.pathname.startsWith("/site")){
+        console.log("IM SIte Middleware");
+    }
 }
