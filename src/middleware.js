e4243f68dc9b97e34653b9e17e7584dc6ee1040e
RazelRaz <razel16dcm@gmail.com>
2023-08-27 16:42:46 +0600

    work with response header manipulate in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,7 @@ import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 
+
 // export function middleware(req,res){
 //     console.log("Middleware");
 // }
@@ -26,25 +27,39 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
-//how to work with response header in middleware
+
 
 
 //how to work with request header mnipulate in middleware
 //if there is an any token avaialable or not
-export function middleware(req,res){
-    if(req.nextUrl.pathname.startsWith("/api/product")){
-        const reqHeaders = new Headers(req.Headers);
-        const token = reqHeaders.get('token')
+// export function middleware(req,res){
+//     if(req.nextUrl.pathname.startsWith("/api/product")){
+//         const reqHeaders = new Headers(req.Headers);
+//         const token = reqHeaders.get('token')
 
-        if(token==="123-XYZ"){
-            //set new header property
-            reqHeaders.set('user_id', '1991')
-            reqHeaders.set('user_email', 'mailrazel@mail.com')
-           return NextResponse.next({
-                request:{headers:reqHeaders}
-           });
-        } else {
-           return NextResponse.json({}, {status:401})
-        }
+//         if(token==="123-XYZ"){
+//             //set new header property
+//             reqHeaders.set('user_id', '1991')
+//             reqHeaders.set('user_email', 'mailrazel@mail.com')
+//            return NextResponse.next({
+//                 request:{headers:reqHeaders}
+//            });
+//         } else {
+//            return NextResponse.json({}, {status:401})
+//         }
+//     }
+// }
+
+//how to work with response header manipulate in middleware
+export function middleware(req,res){
+    const reqHeaders = new Headers(req.Headers);
+    const token = reqHeaders.get('token')
+    if(token==="123-XYZ"){
+       const response = NextResponse.next()
+       response.headers.set('Set-Cookie', 'session=XXXX; expires=Tues, 27 Aug 2023 15:45:00 GMT; path=/')
+       response.headers.set('test', 'test')
+       return response;
+    } else {
+        return NextResponse.json({}, {status:401})
     }
 }
