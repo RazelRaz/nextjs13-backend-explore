b1952da7065b1c64a5c6232acfc2ea5eca5bf954
RazelRaz <razel16dcm@gmail.com>
2023-08-30 12:30:28 +0600

    catch request body with POST

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -3,27 +3,38 @@ import { NextResponse, NextRequest} from "next/server";
 // import { redirect } from 'next/navigation'
 
 //GET
-export async function GET(req,res){
-    const {searchParams} = new URL(req.url)
-    let id = searchParams.get('id')
+// export async function GET(req,res){
+//     const {searchParams} = new URL(req.url)
+//     let id = searchParams.get('id')
 
-    return NextResponse.json({msg: id})
-}
+//     return NextResponse.json({msg: id})
+// }
 
 
 
 
 
 //POST
+// export async function POST(req,res){
+//     const {searchParams} = new URL(req.url);
+//     let id = searchParams.get('id');
+//     let name = searchParams.get('name');
+//     let city = searchParams.get('city');
+
+//     return NextResponse.json({id:id, name:name, city:city})
+// }
+
+
+
+//catch request body with POST
 export async function POST(req,res){
-    const {searchParams} = new URL(req.url);
-    let id = searchParams.get('id');
-    let name = searchParams.get('name');
-    let city = searchParams.get('city');
+    const reqBody = await req.json()
 
-    return NextResponse.json({id:id, name:name, city:city})
+    let name = reqBody['name']
+    let city = reqBody['city']
+
+    return NextResponse.json({msg: name})
 }
-
 
 
 
